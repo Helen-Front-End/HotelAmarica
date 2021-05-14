fd94fce674cd45fd48e585ff8cf821a3c82e625b
Helen-Front-End <66969932+Helen-Front-End@users.noreply.github.com>
2021-05-14 17:35:50 +0300

    HotelAmarica

---
+++ js/script.js
@@ -0,0 +1,61 @@
+/*form*/
+let button = document.querySelector('.button');
+      let buttonText = document.querySelector('.tick');
+
+      const tickMark = "<svg width=\"58\" height=\"45\" viewBox=\"0 0 58 45\" xmlns=\"http://www.w3.org/2000/svg\"><path fill=\"#fff\" fill-rule=\"nonzero\" d=\"M19.11 44.64L.27 25.81l5.66-5.66 13.18 13.18L52.07.38l5.65 5.65\"/></svg>";
+
+      buttonText.innerHTML = "Book now";
+
+      button.addEventListener('click', function() {
+
+        if (buttonText.innerHTML !== "Book now") {
+          buttonText.innerHTML = "Book now";
+        } else if (buttonText.innerHTML === "Book now") {
+          buttonText.innerHTML = tickMark;
+        }
+         this.classList.toggle('button__circle');
+});
+
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 250, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+
+/*slider*/
+
+$(document).ready(function(){
+  $('.slider').slick({
+      arrows:true,
+      dots:true,
+      adaptiveHeight:true,
+      speed:1000,
+      easing:'ease',
+      autoplay:true,
+      autoplaySpeed:2000,
+      waitForAnimate:false,
+      variableWidth:true,
+      centerMode:true
+  });
+});
+
